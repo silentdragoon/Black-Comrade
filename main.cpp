4036cea6b053f4d61513aab54a3656c14d222511
JMC Williams <jw7082@delhi.cs.bris.ac.uk>
2009-11-05 16:34:40 +0000

    Added ogre demo with flying camera

---
+++ main.cpp
@@ -0,0 +1,111 @@
+
+// Includes all Ogre classes
+#include <OGRE/Ogre.h>
+
+#include "ExampleFrameListener.h"
+
+using namespace Ogre;
+
+class Main {
+private:
+    Root *root;
+    Camera *camera;
+    SceneManager *sceneMgr;
+    RenderWindow *window;
+    ExampleFrameListener *frameListener;
+    
+    SceneNode *robotNode;
+    
+    void createCamera();
+    void createScene();
+    void createViewPort();
+    void createFrameListener(void);
+    
+public:
+    Main();
+};
+
+Main::Main() {
+    
+    root = new Root();
+    
+    root->showConfigDialog();
+    
+    window = root->initialise(true, "Test Window");
+    
+    sceneMgr = root->createSceneManager(ST_GENERIC);
+  
+    // Set the paths to look for varius resources
+    ResourceGroupManager::getSingleton().addResourceLocation(
+                    "models", "FileSystem", "General");
+  
+    ResourceGroupManager::getSingleton().addResourceLocation(
+                    "materials/scripts", "FileSystem", "General");
+                    
+    ResourceGroupManager::getSingleton().addResourceLocation(
+                    "materials/programs", "FileSystem", "General");
+                    
+    ResourceGroupManager::getSingleton().addResourceLocation(
+                    "materials/textures", "FileSystem", "General");
+    
+    // Magic Resource line
+    ResourceGroupManager::getSingleton().initialiseAllResourceGroups();
+    
+    createCamera();
+    createViewPort();
+    createScene();
+    
+    createFrameListener();
+    
+    // Start Rendering Loop
+    root->startRendering();
+}
+
+void Main::createCamera() {
+
+    camera = sceneMgr->createCamera("mainCam");
+    
+    camera->setPosition(Vector3(0,0,50));
+    camera->lookAt(Vector3(1,0,50));
+    camera->setNearClipDistance(5);
+    camera->setFarClipDistance(1000);
+}
+
+void Main::createViewPort() {
+
+    Viewport *vp = window->addViewport(camera);
+    vp->setBackgroundColour(ColourValue(100,0,0));
+    
+    camera->setAspectRatio(
+        Real(vp->getActualWidth()) / Real(vp->getActualHeight()));
+}
+
+void Main::createScene() {
+
+    sceneMgr->setAmbientLight(ColourValue(0.5,0.5,0.5));
+    
+    Light *l = sceneMgr->createLight("MainLight");
+    
+    l->setPosition(20,80,50);
+    
+    Entity *e = sceneMgr->createEntity("object","testmap.mesh");
+    
+    //e->setMaterialName("Examples/EnvMappedRustySteel");
+    
+    robotNode = sceneMgr->getRootSceneNode()->createChildSceneNode();
+    robotNode->attachObject(e);
+    
+    robotNode->yaw(Ogre::Radian(4.712));
+}
+
+void Main::createFrameListener(void)
+    {
+        frameListener= new ExampleFrameListener(window, camera);
+        frameListener->showDebugOverlay(true);
+        root->addFrameListener(frameListener);
+    }
+
+int main() {
+    
+    Main *main = new Main();
+}
