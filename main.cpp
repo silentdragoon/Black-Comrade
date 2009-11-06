53ec8be38a8204a63c418594b13d1b6ae42cb04e
James Williams <j@lynx.(none)>
2009-11-06 23:00:43 +0000

    Split main.cpp into main.cpp and main.h

--- main.cpp
+++ main.cpp
@@ -1,29 +1,7 @@
 
-// Includes all Ogre classes
-#include <OGRE/Ogre.h>
-
-#include "ExampleFrameListener.h"
+#include "main.h"
 
 using namespace Ogre;
-
-class Main {
-private:
-    Root *root;
-    Camera *camera;
-    SceneManager *sceneMgr;
-    RenderWindow *window;
-    ExampleFrameListener *frameListener;
-    
-    SceneNode *robotNode;
-    
-    void createCamera();
-    void createScene();
-    void createViewPort();
-    void createFrameListener(void);
-    
-public:
-    Main();
-};
 
 Main::Main() {
     
---
+++ main.h
@@ -0,0 +1,29 @@
+
+#ifndef MAIN_H
+#define MAIN_H
+
+// Includes all Ogre classes
+#include <OGRE/Ogre.h>
+
+#include "ExampleFrameListener.h"
+
+class Main {
+private:
+    Root *root;
+    Camera *camera;
+    SceneManager *sceneMgr;
+    RenderWindow *window;
+    ExampleFrameListener *frameListener;
+    
+    SceneNode *robotNode;
+    
+    void createCamera();
+    void createScene();
+    void createViewPort();
+    void createFrameListener(void);
+    
+public:
+    Main();
+};
+
+#endif
