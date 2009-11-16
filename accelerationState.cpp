03771a1ba7bfd68cd4df4ac32a9896aabf9dd900
JS Kestner <jk7272@dhaka.cs.bris.ac.uk>
2009-11-16 20:06:43 +0000

    flying now compiles and outputs a value up to one and acceleration can be changed by changing the constants in the headerfile.

--- accelerationState.cpp
+++ accelerationState.cpp
@@ -6,7 +6,12 @@ AccelerationState::AccelerationState( IAccelerationState *fromInput ):
     vPropSideVel(0.0),
     vPropUpVel(0.0),
     vPropYawVel(0.0),
-    vPropPitchVel(0.0)
+    vPropPitchVel(0.0),
+    cForward(0.0),
+    cSide(0.0),
+    cUp(0.0),
+    cYaw(0.0),
+    cPitch(0.0)
 {}
 
 double AccelerationState::forward()
@@ -34,14 +39,57 @@ double AccelerationState::pitch()
     return vPropPitchVel;
 }
 
+double calc( double val, double aConst )
+{
+    if( val > 0 ) return (-exp(-val/(0.35*aConst)) + 1);
+    else return -(-exp(val/(0.35*aConst)) + 1);
+}
+
+void doTick( double in, double &counter, double &propVel, double BRAKE, double ACCEL)
+{
+    if( in == 0 ) counter = BRAKE * counter;
+    else counter += in;
+    propVel = calc( counter, ACCEL);
+}
+
+
+
 void AccelerationState::tick()
 {
+    doTick(input->forward(), cForward, vPropForwardVel, 0.8, TICKSFORWARD);
+
+    doTick(input->side(), cSide, vPropSideVel, 0.8, TICKSSIDE);
     
-    vPropForwardVel = input->forward();
-    vPropSideVel = input->side();
-    vPropUpVel = input->up();
-    vPropYawVel = input->yaw();
-    vPropPitchVel = input->pitch();
+    doTick(input->up(), cUp, vPropUpVel, 0.8, TICKSUP);
     
+    doTick(input->yaw(), cYaw, vPropYawVel, 0.8, TICKSYAW);
     
+    doTick(input->pitch(), cPitch, vPropPitchVel, 0.8, TICKSPITCH);
+    
+    /*    
+    double tmp = input->forward();
+    if( tmp == 0 ) cForward = 0.9 * cForward;
+    cForward += tmp;
+    vPropForwardVel = calc( cForward, TICKSFORWARD );
+    
+    tmp = input->side();
+    if( tmp == 0 ) cSide = 0.9 * cSide;
+    cSide += tmp;
+    vPropSideVel = calc( cSide, TICKSSIDE );
+    
+    tmp = input->up();
+    if( tmp == 0 ) cUp = 0.9 * cUp;
+    cUp += tmp;
+    vPropUpVel = calc( cUp, TICKSUP );
+    
+    tmp = input->yaw();
+    if( tmp == 0 ) cYaw = 0.6 * cYaw;
+    cYaw += tmp;
+    vPropYawVel = calc( cYaw, TICKSYAW );
+    
+    tmp = input->pitch();
+    if( tmp == 0 ) cPitch = 0.6 * cPitch;
+    cPitch += tmp;
+    vPropPitchVel = calc( cPitch, TICKSYAW );
+    */
 }
--- accelerationState.h
+++ accelerationState.h
@@ -5,16 +5,34 @@
 
 #include "ITickable.h"
 #include "IAccelerationState.h"
+#include <math.h>
+#include <iostream>
+
+using namespace std;
 
 class AccelerationState : public IAccelerationState, public ITickable
 {
   private:
     double vPropForwardVel;
+    double cforward;
     double vPropSideVel;
     double vPropUpVel;
     double vPropYawVel;
     double vPropPitchVel;
     IAccelerationState *input;
+    
+    double cForward;
+    double cSide;
+    double cUp;
+    double cYaw;
+    double cPitch;
+    
+    //ticks required to reach ~95%
+    static const double TICKSFORWARD = 200;
+    static const double TICKSSIDE = 100;
+    static const double TICKSUP = 50;
+    static const double TICKSYAW = 20;
+    static const double TICKSPITCH = 20;
     
   public:
     virtual double forward();
