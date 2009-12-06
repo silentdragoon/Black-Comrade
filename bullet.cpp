4d0b2a1fa7f1496b7fa3067584e3101d97e6c01a
josh <josh@phenom.localdomain>
2009-12-06 01:30:47 +0000

    Forgot to add the files

---
+++ bullet.cpp
@@ -0,0 +1,32 @@
+#include "bullet.h"
+
+void Bullet::updateLocation() {
+    // Assuming direction is some sort of unit vector or something
+    bulletNode->setPosition(Vector3((direction.x)*velocity,(direction.y)*velocity,(direction.z)*velocity));
+
+    aliveTicks++;
+}
+
+void Bullet::setLightColor(Vector3 lc) {
+    lightColor = lc;
+}
+
+void Bullet::setTrailColor(Vector3 tc) {
+    trailColor = tc;
+}
+
+Bullet::Bullet(SceneNode *bulletNode,
+        Vector3 direction,
+        int velocity,
+        bool light,
+        bool trail)
+        : 
+            bulletNode(bulletNode),
+            direction(direction),
+            velocity(velocity),
+            light(light),
+            trail(trail),
+            aliveTicks(0)
+{
+}
+        
---
+++ bullet.h
@@ -0,0 +1,40 @@
+#ifndef _BULLET_H 
+#define _BULLET_H
+
+#include <OGRE/Ogre.h>
+#include <string>
+
+using namespace Ogre;
+using namespace std;
+
+class Bullet
+{
+    private:
+        SceneNode *bulletNode;
+
+        Vector3 direction;
+        int velocity;
+
+        bool light;
+        Vector3 lightColor;
+        
+        bool trail;
+        Vector3 trailColor;
+
+        int aliveTicks;        
+   public:
+        Bullet(SceneNode *bulletNode,
+            Vector3 direction, 
+            int velocity,
+            bool light,
+            bool trail);
+
+        // TODO: Add destructor
+
+        void updateLocation();
+        void setLightColor(Vector3 lc);
+        void setTrailColor(Vector3 tc);
+};
+
+#endif
+
