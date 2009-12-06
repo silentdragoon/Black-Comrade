5dbd53a492ba7ed84e8c54dfe47b208f5b9f7290
josh <josh@phenom.localdomain>
2009-12-06 01:26:40 +0000

    Might work, but I cant tell

--- bulletManager.cpp
+++ bulletManager.cpp
@@ -19,6 +19,11 @@ void BulletManager::updateBullets() {
     // Update the position of all the bullets in the vector. Somehow test to see if they
     // have collided with anything, or if they have run their course. If this is the case
     // we need to destroy them and remove them from the vector.
+    for(int i=0;i<activeBullets.size();i++) {
+        Bullet b = activeBullets.at(i);
+        b.updateLocation();
+        // TODO: Test for a collision/Destruction here?
+    }
 }
 
 BulletManager::BulletManager(SceneNode *shipSceneNode,SceneManager *sceneMgr, FrontGunState *gunState) 
