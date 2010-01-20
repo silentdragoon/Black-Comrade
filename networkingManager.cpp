ab22f3a40970db2d3abb4f5b279c8ce48570b3eb
RT Paton <rp7374@santiago.cs.bris.ac.uk>
2010-01-20 17:14:32 +0000

    Changed autoserialize time on networking.

--- networkingManager.cpp
+++ networkingManager.cpp
@@ -17,7 +17,7 @@ NetworkingManager::NetworkingManager(IExit *mExit) :
 {
     sd.port = 0;
     replicaManager.SetDefaultPacketReliability(RELIABLE_ORDERED);
-    replicaManager.SetAutoSerializeInterval(1);
+    replicaManager.SetAutoSerializeInterval(10);
 }
 
 NetworkingManager::~NetworkingManager() {
