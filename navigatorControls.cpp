35c353e6c70a378ee8e885687595fc5ef456207c
Robert <robert@robert-desktop.(none)>
2010-02-01 21:14:08 +0000

    Fixed conflict in navigatorControls.cpp

--- navigatorControls.cpp
+++ navigatorControls.cpp
@@ -1,9 +1,5 @@
 #include "navigatorControls.h"
-<<<<<<< HEAD:navigatorControls.cpp
 #include "const.h"
-=======
-#include <iostream>
->>>>>>> 84224194c65ec8f50f96274c654977febdd37848:navigatorControls.cpp
 
 NavigatorControls::NavigatorControls(InputState *inputState, Camera *cam) :
     inputState(inputState),
@@ -19,13 +15,8 @@ void NavigatorControls::tick()
     if(enabled) {
         int x = inputState->getMouseX();
         int y = inputState->getMouseY();
-<<<<<<< HEAD:navigatorControls.cpp
+
         cam->yaw(Degree(Const::TURRET_SPEED * x));
         cam->pitch(Degree(Const::TURRET_SPEED * y));
-=======
-
-        cam->yaw(Degree(-0.13 * x));
-        cam->pitch(Degree(-0.13 * y));
->>>>>>> 84224194c65ec8f50f96274c654977febdd37848:navigatorControls.cpp
     }
 }
