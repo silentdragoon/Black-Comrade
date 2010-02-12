56864bf437033195a036565bf8e430a5ba325977
JMC Williams <jw7082@kyiv.cs.bris.ac.uk>
2010-02-12 18:06:02 +0000

    Map created and centered

--- guiManager.cpp
+++ guiManager.cpp
@@ -54,6 +54,10 @@ CEGUI::FrameWindow* GuiManager::buildMinimap() {
     // GET MAP PARTS HERE WITH A LOOOOOOP
     // -------------------------------------------------------------------
 
+    int winWidth = Ogre::Root::getSingleton().getAutoCreatedWindow()->getWidth();
+    int winHeight= Ogre::Root::getSingleton().getAutoCreatedWindow()->getHeight();
+    float ratio = winWidth / (float)winHeight;
+
     for(int xpos=0;xpos<Const::MAPSIZE;xpos++) {
         for(int ypos=0;ypos<Const::MAPSIZE;ypos++) {
             CEGUI::ImageryComponent ic = CEGUI::ImageryComponent();
@@ -77,13 +81,22 @@ CEGUI::FrameWindow* GuiManager::buildMinimap() {
                 string name = tile.str();
                 ic.setImage("Minimap",name);
             }
+            ic.setVerticalFormatting(CEGUI::VF_STRETCHED);
+            ic.setHorizontalFormatting(CEGUI::HF_STRETCHED);
 
             // TODO: SCALING HERE NEEDS FIXING SO IT DOESNT TAKE UP ALL OF THE SCREEEEN
             CEGUI::ComponentArea ca = CEGUI::ComponentArea();
+            /*
             ca.d_left = CEGUI::Dimension(CEGUI::AbsoluteDim(xpos*30),CEGUI::DT_X_POSITION);
             ca.d_top = CEGUI::Dimension(CEGUI::AbsoluteDim(ypos*30),CEGUI::DT_Y_POSITION);
             ca.d_right_or_width = CEGUI::Dimension(CEGUI::AbsoluteDim(30),CEGUI::DT_WIDTH);
             ca.d_bottom_or_height = CEGUI::Dimension(CEGUI::AbsoluteDim(30),CEGUI::DT_HEIGHT);
+            */
+
+            ca.d_left = CEGUI::Dimension(CEGUI::UnifiedDim(CEGUI::UDim(xpos*0.015/ratio,0),CEGUI::DT_X_POSITION),CEGUI::DT_X_POSITION);
+            ca.d_top = CEGUI::Dimension(CEGUI::UnifiedDim(CEGUI::UDim(ypos*0.015,0),CEGUI::DT_Y_POSITION),CEGUI::DT_Y_POSITION);
+            ca.d_right_or_width = CEGUI::Dimension(CEGUI::UnifiedDim(CEGUI::UDim(0.015/ratio,0),CEGUI::DT_WIDTH),CEGUI::DT_WIDTH);
+            ca.d_bottom_or_height = CEGUI::Dimension(CEGUI::UnifiedDim(CEGUI::UDim(0.015,0),CEGUI::DT_HEIGHT),CEGUI::DT_HEIGHT);
 
             ic.setComponentArea(ca);
 
@@ -111,8 +124,9 @@ CEGUI::FrameWindow* GuiManager::buildMinimap() {
     // Create the FrameWindow to return
     CEGUI::FrameWindow *minimap = static_cast<CEGUI::FrameWindow*>(guiMgr->createWindow("BlackComrade/CrossHair","mini"));
     minimap->setLookNFeel(lookFeel.getName());
-    minimap->setPosition(CEGUI::UVector2(CEGUI::UDim(0.8f,0),CEGUI::UDim(0.8f,0)));
-    minimap->setSize(CEGUI::UVector2(CEGUI::UDim(0.2f,0),CEGUI::UDim(0.2f,0)));
+    minimap->setPosition(CEGUI::UVector2(CEGUI::UDim(0.5-30*0.015/ratio/2,0),CEGUI::UDim(0.5-30*0.015/2,0)));
+    //minimap->setPosition(CEGUI::UVector2(CEGUI::UDim(0,0),CEGUI::UDim(0,0)));
+    minimap->setSize(CEGUI::UVector2(CEGUI::UDim(1.0f,0),CEGUI::UDim(1.0f,0)));
 
     return minimap;
 }
