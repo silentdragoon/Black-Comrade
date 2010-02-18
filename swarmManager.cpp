2d7997dfcd62a9803587da5ecdf39c5ce98151cf
J Fyne <jf5329@beijing.cs.bris.ac.uk>
2010-02-18 13:56:51 +0000

    Another tesst

--- swarmManager.cpp
+++ swarmManager.cpp
@@ -141,7 +141,7 @@ void SwarmManager::tick()
             if(swarmTick>Const::SPAWN_DELAY) {
                 swarmTick = 0;
                 Vector3 spawnPoint = mapMgr->getDynamicSpawnPoint(shipState->getPosition());
-                createSwarm(10,spawnPoint);
+                createSwarm(5,spawnPoint);
                 cout << "Swarms: " << activeSwarms.size() << endl;
             }
         }
