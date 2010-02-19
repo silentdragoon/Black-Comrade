e77992deabbd28fdb7cce50d7f60e16deee936a5
Robert <robert@robert-desktop.(none)>
2010-02-19 00:24:38 +0000

    Notification system now working (still need to link it in with audio state, GUI etc

--- notificationManager.cpp
+++ notificationManager.cpp
@@ -3,148 +3,198 @@
 
 #include <iostream>
 
-NotificationManager::NotificationManager(GameStateMachine *stateMachine, MapManager *mapManager, ShipState *shipState, DamageState *damageState)
-	: notification(NT_NONE)
-    , oldNotification(NT_NONE)
+NotificationManager::NotificationManager(CollaborationInfo *collabInfo, GameStateMachine *stateMachine,
+                                         MapManager *mapManager, ShipState *shipState,
+                                         DamageState *damageState)
+    : collabInfo(collabInfo)
+    , notification(NT_NONE)
+    , lastNotification(NT_NONE)
     , stateMachine(stateMachine)
-	, mapManager(mapManager)
-	, shipState(shipState)
+    , mapManager(mapManager)
+    , shipState(shipState)
     , damageState(damageState)
-	, mIsNewNotification(false)
-    , tickcount(0)
+    , mIsNewNotification(false)
+    , lastStateNotified(GS_END)
+    , controlsDisplayed(false)
+    , tickcount(2000)
 {
-    queue = std::vector<Notification>();
-    recency = std::map<Notification,int>();
-
-    int max = std::numeric_limits<int>::max();
-    recency.insert(std::pair<Notification,int>(NT_CONTROLS,max));
-    recency.insert(std::pair<Notification,int>(NT_ENGINES_CRITICAL,max));
-    recency.insert(std::pair<Notification,int>(NT_WEAPONS_CRITICAL,max));
-    recency.insert(std::pair<Notification,int>(NT_HULL_CRITICAL,max));
-    recency.insert(std::pair<Notification,int>(NT_ATTACK,max));
+    maxDelay = std::numeric_limits<int>::max();
+    recency.insert(std::pair<Notification,int>(NT_CONTROLS,maxDelay));
+    recency.insert(std::pair<Notification,int>(NT_ENGINES_CRITICAL,maxDelay));
+    recency.insert(std::pair<Notification,int>(NT_WEAPONS_CRITICAL,maxDelay));
+    recency.insert(std::pair<Notification,int>(NT_HULL_CRITICAL,maxDelay));
+    recency.insert(std::pair<Notification,int>(NT_UNDER_ATTACK,maxDelay));
+    recency.insert(std::pair<Notification,int>(NT_COMMENT_ONE,maxDelay));
+    recency.insert(std::pair<Notification,int>(NT_COMMENT_TWO,maxDelay));
+    recency.insert(std::pair<Notification,int>(NT_COMMENT_THREE,maxDelay));
+    recency.insert(std::pair<Notification,int>(NT_OBJECTIVE_SEEK,maxDelay));
+    recency.insert(std::pair<Notification,int>(NT_OBJECTIVE_DESTROY,maxDelay));
+    recency.insert(std::pair<Notification,int>(NT_OBJECTIVE_ESCAPE,maxDelay));
 }
 
 NotificationManager::NotificationManager()
     : notification(NT_NONE)
-    , oldNotification(NT_NONE)
+    , lastNotification(NT_NONE)
+    , collabInfo(0)
     , stateMachine(0)
     , mapManager(0)
     , shipState(0)
     , damageState(0)
     , mIsNewNotification(false)
-{
-    queue = std::vector<Notification>();
-}
+{}
 
 void NotificationManager::updateRecencies() {
-    if (tickcount < 1 / ConstManager::getFloat("tick_period")) {
-        tickcount ++;
-    } else {
-        tickcount = 0;
-        std::map<Notification,int>::const_iterator itr;
-        for (itr = recency.begin() ; itr != recency.end() ; itr++) {
-            if (itr->second == std::numeric_limits<int>::max()) break;
-            recency[itr->first] = itr->second + 1;
-            //std::cout << itr->second << std::endl;
-        }
+
+    std::map<Notification,int>::const_iterator itr;
+    for (itr = recency.begin() ; itr != recency.end() ; itr++) {
+        if (itr->second != maxDelay) recency[itr->first] = itr->second + 1;
     }
 }
 
 void NotificationManager::tick()
 {
-	mIsNewNotification = false;
-    updateRecencies();
+    if (collabInfo == 0) {
+        std::cerr << "NotificationManager has no collabinfo" << std::endl;
+        exit(-1);
+    }
+
+    mIsNewNotification = false;
 
     if (mapManager == 0) {
-        if (oldNotification != notification) {
+        if (lastNotification != notification) {
             mIsNewNotification = true;
             notify();
-            if (notification != NT_NONE) oldNotification = notification;
+            if (notification != NT_NONE) lastNotification = notification;
         }
         return;
     }
 
-    //std::cout << "Checking statemachine" << std::endl;
+    if (tickcount < 1 / ConstManager::getFloat("tick_period")) {
+        tickcount ++;
+    } else {
+        tickcount = 0;
+        updateRecencies();
+        checkComments(); 
+        checkShipPosition();
+        checkHealth();
+        checkGameState();
 
-    //checkShipPosition();
-    checkHealth();
-    checkGameState();
-    notify();
+        notify();
+    }
 }
 
 void NotificationManager::notify() {
 
-    //if (oldNotification == notification) return;
     switch(notification) {
         case NT_CONTROLS:
-            std::cout << "Here is how you use your controls..." << std::endl;
+            std::cout << "----------> Here's a reminder of your " << collabInfo->getGameRoleString()
+                      << " controls, " << collabInfo->getNick() << std::endl;
             break;
-        case NT_ATTACK:
-            std::cout << "You are under attack! Get ready for some intense s**t." << std::endl;
+        case NT_UNDER_ATTACK:
+            std::cout << "----------> They're coming for us! You may want to increase shield and weapon power..." << std::endl;
             break;
         case NT_ENGINES_CRITICAL:
-            std::cout << "Engines are critical! Repair them quickly, or you'll be immobilised." << std::endl;
+            std::cout << "----------> Engines are critical! Repair them quickly, or you'll be immobilised." << std::endl;
             break;
         case NT_WEAPONS_CRITICAL:
-            std::cout << "Weapons are critical! Repair them quickly, or you'll be defenceless." << std::endl;
+            std::cout << "----------> Weapons are critical! Repair them quickly, or you'll be defenceless." << std::endl;
             break;
         case NT_HULL_CRITICAL:
-            std::cout << "The hull is almost breached! Repair it quickly, or it's game over, men." << std::endl;
+            std::cout << "----------> The hull is almost breached! Repair it quickly, or it's game over, men." << std::endl;
+            break;
+        case NT_COMMENT_ONE:
+            std::cout << "----------> Random comment 1." << std::endl;
+            break;
+        case NT_COMMENT_TWO:
+            std::cout << "----------> Random comment 2." << std::endl;
+            break;
+        case NT_COMMENT_THREE:
+            std::cout << "----------> Random comment 3." << std::endl;
             break;
     }
-    if (notification != NT_NONE) oldNotification = notification;
-    recency[notification] = 0;
-    notification = NT_NONE;
-    //std::cout << recency[notification] << std::endl;
+    if (notification != NT_NONE) {
+        lastNotification = notification;
+        notification = NT_NONE;
+        recency[notification] = 0;
+    }
+}
+
+bool NotificationManager::isTimely(Notification notification, int delaySinceMe, int delaySinceLast) {
+    return (getTimeSince(notification) > delaySinceMe && getTimeSinceLast() > delaySinceLast);
 }
 
 int NotificationManager::getTimeSince(Notification notification) {
     std::map<Notification, int>::const_iterator itr;
     itr = recency.find(notification);
     if (itr == recency.end()) {
-        //Entity has not bee created
         return 0;
     } else {
         return itr->second;
     }
 }
 
+int NotificationManager::getTimeSinceLast() {
+    int last = std::numeric_limits<int>::max();
+    std::map<Notification,int>::const_iterator itr;
+    for (itr = recency.begin() ; itr != recency.end() ; itr++) {
+        if (itr->second < last) last = itr->second;
+    }
+    return last;
+}
+
 void NotificationManager::checkGameState() {
     GameState gameState = stateMachine->currentGameState();
-    Notification newNotification;
+    if (lastStateNotified == gameState) return;
+    Notification newNotification = lastNotification;
     switch(gameState) {
         case GS_STEALTH:
-            newNotification = NT_CONTROLS;
+            if (!controlsDisplayed)
+                newNotification = NT_CONTROLS;
+                controlsDisplayed = true;
             break;
         case GS_ATTACK:
-            newNotification = NT_ATTACK;
+            newNotification = NT_UNDER_ATTACK;
             break;
     }
-	if(oldNotification != newNotification) {
-        //std::cout << getTimeSince(newNotification) << std::endl;
-        if (getTimeSince(newNotification) > 20) {
+    if(lastNotification != newNotification) {
+        if (isTimely(newNotification,0,8)) {
+            mIsNewNotification = true;
+            notification = newNotification;
+            lastStateNotified = gameState;
+        }
+    }
+}
+
+void NotificationManager::checkShipPosition() {
+    //TODO - Base any notifications on map tiles?
+}
+
+void NotificationManager::checkComments() {
+    srand ( time(NULL) );
+    int irand = rand() % 3 + 1;
+
+    Notification newNotification = lastNotification;
+    switch(irand) {
+        case 1:
+            newNotification = NT_COMMENT_ONE;
+            break;
+        case 2:
+            newNotification = NT_COMMENT_TWO;
+            break;
+        case 3:
+            newNotification = NT_COMMENT_THREE;
+            break;
+    }
+    if(lastNotification != newNotification) {
+        if (isTimely(newNotification,180,30)) {
             mIsNewNotification = true;
             notification = newNotification;
         }
     }
 }
 
-void NotificationManager::checkShipPosition() {
-    // Waypoint events
-    //string *wp = mapManager->getWaypoint(shipState->getPosition());
-    //if(wp != NULL) {
-    //    if(*wp == "wp_attack") {
-    //        switch(gameState) {
-    //            case GS_STEALTH:
-    //            gameState = GS_ATTACK;
-    //            break;
-    //        }
-    //    }
-    //}
-}
-
 void NotificationManager::checkHealth() {
-    Notification newNotification;
+    Notification newNotification = lastNotification;
     if (damageState->getHullHealth() <= 20) {
         newNotification = NT_HULL_CRITICAL;
     } else if (damageState->getEngineHealth() <= 20) {
@@ -152,26 +202,30 @@ void NotificationManager::checkHealth() {
     } else if (damageState->getWeaponHealth() <= 20) {
         newNotification = NT_WEAPONS_CRITICAL;
     }
-	if(oldNotification != newNotification) {
-        if (getTimeSince(newNotification) > 20) {
+    if(lastNotification != newNotification) {
+        if (isTimely(newNotification,20,8)) {
             mIsNewNotification = true;
             notification = newNotification;
         }
     }
 }
+
+void NotificationManager::setCollaborationInfo(CollaborationInfo *mCollabInfo) {
+    collabInfo = mCollabInfo;
+}
 	
-Notification NotificationManager::currentNotification() {
-	return notification;
+Notification NotificationManager::getCurrentNotification() {
+	return lastNotification;
 }
 
-bool NotificationManager::isNewNotification() {
+bool NotificationManager::hasNewNotification() {
 	return mIsNewNotification;
 }
 
 RakNet::RakString NotificationManager::GetName(void) const {return RakNet::RakString("NotificationManager");}
 
 RM3SerializationResult NotificationManager::Serialize(SerializeParameters *serializeParameters) {
-    serializeParameters->outputBitstream[0].Write(notification);
+    serializeParameters->outputBitstream[0].Write(lastNotification);
     return RM3SR_BROADCAST_IDENTICALLY;
 }
 
--- notificationManager.h
+++ notificationManager.h
@@ -10,43 +10,61 @@
 #include "constManager.h"
 #include <vector>
 #include <limits>
+#include <cstdlib>
 
 #include "replicaObject.h"
+#include "collaborationInfo.h"
 
-enum Notification { NT_NONE, NT_CONTROLS, NT_ATTACK, NT_HULL_CRITICAL, NT_ENGINES_CRITICAL, NT_WEAPONS_CRITICAL };
+enum Notification { NT_NONE, NT_CONTROLS, NT_UNDER_ATTACK,
+                    NT_HULL_CRITICAL, NT_ENGINES_CRITICAL, NT_WEAPONS_CRITICAL,
+                    NT_OBJECTIVE_SEEK, NT_OBJECTIVE_DESTROY, NT_OBJECTIVE_ESCAPE,
+                    NT_COMMENT_ONE, NT_COMMENT_TWO, NT_COMMENT_THREE };
 
 class NotificationManager : public ITickable, public ReplicaObject
 {
 private:
-	Notification notification;
-    Notification oldNotification;
+    Notification notification;
+    Notification lastNotification;
 
     std::vector<Notification> queue;
     std::map <Notification,int> recency;
 
+    CollaborationInfo *collabInfo;
     GameStateMachine *stateMachine;
-	MapManager *mapManager;
-	ShipState *shipState;
+    GameState lastStateNotified;
+    MapManager *mapManager;
+    ShipState *shipState;
     DamageState *damageState;
-	bool mIsNewNotification;
+    bool mIsNewNotification;
 
     int tickcount;
+    int maxDelay;
 
+    bool controlsDisplayed;
+    bool stateChangePending;
+
+    void checkComments();
     void checkGameState();
     void checkShipPosition();
     void checkHealth();
 
+    bool isTimely(Notification notification, int delaySinceMe, int delaySinceLast);
     int getTimeSince(Notification notification);
+    int getTimeSinceLast();
     void updateRecencies();
     void notify();
 	
 public:
     NotificationManager();
-	NotificationManager(GameStateMachine *stateMachine, MapManager *mapManager, ShipState *shipState, DamageState *damageState);
-	void tick();
+    NotificationManager(CollaborationInfo *collabInfo, GameStateMachine *stateMachine,
+                        MapManager *mapManager, ShipState *shipState,
+                        DamageState *damageState);
+    void tick();
 	
-	Notification currentNotification();
-	bool isNewNotification();
+    Notification getCurrentNotification();
+    bool hasNewNotification();
+
+    void setCollaborationInfo(CollaborationInfo *collabInfo);
 
     virtual RakNet::RakString GetName(void) const;
     virtual RM3SerializationResult Serialize(SerializeParameters *serializeParameters);
