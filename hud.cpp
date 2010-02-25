7b0cf8715ffcd721b8651e8e9bcd1f9718a449e7
WS Judd <wj7805@moscow.cs.bris.ac.uk>
2010-02-25 13:03:29 +0000

    Added more GUI elements, added non-centred add image function for pixel perfect layout, added pixel values to hud

--- hud.cpp
+++ hud.cpp
@@ -13,8 +13,25 @@ HUD::HUD(GuiManager *guiManager, ShipState *shipState)
     int winWidth = Ogre::Root::getSingleton().getAutoCreatedWindow()->getWidth();
     int winHeight= Ogre::Root::getSingleton().getAutoCreatedWindow()->getHeight();
     float ratio = winWidth / (float)winHeight;
+    float noscale = 1.6 / ratio;
 
+    float wpixel = 1.0 / (float)winWidth;
+    float hpixel = 1.0 / (float)winHeight;
 
-    CEGUI::FrameWindow *xcrosshair = guiManager->addStaticImage("XCrosshair", 0.5, 0.5, 0.05/ratio, 0.05, "XCrosshair", "XCross");
-    CEGUI::FrameWindow *overlay = guiManager->addStaticImage("Overlay", 0.5, 0.5, 1.6/ratio, 1.0, "Overlay", "Overlay");
+    // Background Elements
+    CEGUI::FrameWindow *xcrosshair  = guiManager->addStaticImage("XCrosshair",  0.5, 0.5,   0.05/ratio, 0.05,   "XCrosshair",   "XCross"  );
+    CEGUI::FrameWindow *overlay     = guiManager->addStaticImage("Overlay",     0.5, 0.5,   1.6/ratio,  1.0,    "Overlay",      "Overlay" );
+
+    // Foreground Elements
+
+    // Crew Background
+    CEGUI::FrameWindow *crewlb      = guiManager->addStaticImagePix("Crewl",        0.0,                 0.0,                104 * wpixel, 157 * hpixel,  "Crew",        "EmptyBox" );
+    CEGUI::FrameWindow *crewrb      = guiManager->addStaticImagePix("Crewr",        1.0 - 104 * wpixel,  0.0,                104 * wpixel, 157 * hpixel,  "Crew",        "EmptyBox" );
+
+    // Crew Avatars
+    CEGUI::FrameWindow *crewav1     = guiManager->addStaticImagePix("Crewav1",      0.0 + 2 * wpixel,    0.0 + 2 * hpixel,   100 * wpixel, 100 * hpixel, "Crew",        "Avatar1"  );
+    CEGUI::FrameWindow *crewav2     = guiManager->addStaticImagePix("Crewav2",      1.0 - 102 * wpixel,  0.0 + 2 * hpixel,   100 * wpixel, 100 * hpixel, "Crew",        "Avatar2"  );
+
+    // Left
+    CEGUI::FrameWindow *left        = guiManager->addStaticImagePix("Left",         0.0,                 1.0 - 263 * hpixel, 563 * wpixel, 263 * hpixel, "Left",        "Main"  );
 }
