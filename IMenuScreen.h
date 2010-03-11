191e341c64d6413a2fd5181268f8b151cb3b3d60
RT Paton <rp7374@bern.cs.bris.ac.uk>
2010-03-11 13:28:28 +0000

    Added interface for menu screens

---
+++ IMenuScreen.h
@@ -0,0 +1,24 @@
+
+#ifndef MENU_SCREEN_H
+#define MENU_SCREEN_H
+
+#include <OGRE/Ogre.h>
+#include <OIS/OIS.h>
+#include "IConsoleInputReceiver.h"
+
+using namespace Ogre;
+
+class IMenuScreen : public IConsoleInputReceiver {
+public:
+    virtual void tick() = 0;
+    virtual bool end() = 0;
+
+    virtual void show() = 0;
+    virtual void hide() = 0;
+
+    virtual void alphaNumKeyPressed(const OIS::KeyEvent &arg) {};
+    virtual void returnKeyPressed() {};
+    virtual void backspaceKeyPressed() {};
+};
+
+#endif
