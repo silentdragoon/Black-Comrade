096f8d81ea2c654d79d04184768ed95334c3ebf7
RT Paton <rp7374@yerevan.cs.bris.ac.uk>
2010-03-22 11:36:08 +0000

    Made small change to swarmManager

--- swarmManager.cpp
+++ swarmManager.cpp
@@ -123,13 +123,15 @@ void SwarmManager::updateRemoteSwarms() {
         std::vector<ReplicaObject*> replicatedEnemies = networkingMgr->getReplicas("Enemy");
         for (std::vector<ReplicaObject*>::const_iterator it=replicatedEnemies.begin();it!=replicatedEnemies.end();++it) {
             Enemy *enemy = (Enemy*) *it;
-            sceneNodeMgr->createNode(enemy);
+            std::cout << enemy->health << "\n";
             if (enemy->health < 0) {
                 //Make Explosion here
                 Vector3 *pos = enemy->getPosition();
                 particleSystemEffectManager->createExplosion(*pos);
                 sceneNodeMgr->deleteNode(enemy);
                 delete enemy;
+            } else {
+                sceneNodeMgr->createNode(enemy);
             }
         }
     } else {
