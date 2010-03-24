68773ef763b53648e905c5939bb16c9ea6ad5772
JMC Williams <jw7082@praia.cs.bris.ac.uk>
2010-03-24 11:33:28 +0000

    Added missing files (fixMiniGame)

---
+++ fixMiniGame.cpp
@@ -0,0 +1,49 @@
+
+#include "fixMiniGame.h"
+
+FixMiniGame::FixMiniGame()
+{
+    system = SS_NONE;
+    isEnd = false;
+}
+
+void FixMiniGame::tick()
+{
+    switch(system) {
+        case SS_NONE:
+            system = SS_SHIELD_GENERATOR;
+            break;
+        case SS_SHIELD_GENERATOR:
+            system = SS_ENGINES;
+            break;
+        case SS_ENGINES:
+            system = SS_WEAPONS;
+            break;
+        case SS_WEAPONS:
+            system = SS_SENSORS;
+            break;
+        case SS_SENSORS:
+            isEnd = true;
+            break;
+    }
+}
+
+bool FixMiniGame::end()
+{
+    return isEnd;
+}
+
+int FixMiniGame::getScore()
+{
+    return 100;
+}
+
+ShipSystem FixMiniGame::getSystem()
+{
+    return system;
+    std::cout << system << std::endl;
+}
+
+void FixMiniGame::returnKeyPressed() {}
+
+FixMiniGame::~FixMiniGame() {}
