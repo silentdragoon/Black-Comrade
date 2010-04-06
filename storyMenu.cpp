d217c22fbc5801ae517c46d05987a6e0c39fb4da
Robert <robert@robert-desktop.(none)>
2010-04-06 22:19:08 +0100

    Added forgotten files

---
+++ storyMenu.cpp
@@ -0,0 +1,62 @@
+#include "storyMenu.h"
+
+StoryMenu::StoryMenu(InputState *inputState,
+                           NetworkingManager *networkingMgr,
+                           GuiManager *guiMgr)
+    : inputState(inputState)
+    , networkingMgr(networkingMgr)
+    , guiMgr(guiMgr)
+    , isVisible(false)
+    , isEnd(false)
+{
+    CEGUI::ImagesetManager::getSingleton().create("story.xml");
+}
+
+void StoryMenu::tick() {
+    // Check for key presses etc
+    if (inputState->isKeyDown(OIS::KC_RETURN)) {
+        if (!nickBox->getText().empty()) {
+            networkingMgr->nick = nickBox->getText().c_str();
+            isEnd = true;
+        }
+    } else if (inputState->isKeyDown(OIS::KC_ESCAPE)) {
+        std::exit(0);
+    }
+}
+
+void StoryMenu::alphaNumKeyPressed(const OIS::KeyEvent &arg) {
+    if (arg.text != ' ') {
+        CEGUI::System::getSingleton().injectKeyDown(arg.key);
+        CEGUI::System::getSingleton().injectChar(arg.text);
+    }
+}
+
+MenuType StoryMenu::nextMenu() {
+    return MT_CHOOSE_NETWORK_ROLE;
+}
+
+void StoryMenu::show() {
+    // Show background image etc
+    if (isVisible) return;
+
+    guiMgr->addStaticImage("Story",0.5, 0.5,1.0, 1.0,"Story","Whole");
+
+    nickBox = static_cast<CEGUI::Editbox*>(CEGUI::WindowManager::getSingletonPtr()->createWindow("BlackComrade/IEditbox","nickBox"));
+    guiMgr->getRootWindow()->addChildWindow(nickBox);
+    nickBox->setSize(CEGUI::UVector2(CEGUI::UDim(0,100),CEGUI::UDim(0,100)));
+    nickBox->setPosition(CEGUI::UVector2(CEGUI::UDim(0.5,0),CEGUI::UDim(0.5,0)));
+    nickBox->setMaxTextLength(10);
+    nickBox->activate();
+
+    isVisible = true;
+}
+
+void StoryMenu::hide() {
+    // Hide background image etc
+    CEGUI::WindowManager::getSingletonPtr()->destroyWindow("Story");
+    CEGUI::WindowManager::getSingletonPtr()->destroyWindow(nickBox);
+}
+
+bool StoryMenu::end() { return isEnd; }
+
+bool StoryMenu::visible() { return isVisible; }
---
+++ storyMenu.h
@@ -0,0 +1,40 @@
+#ifndef STORY_MENU_H
+#define STORY_MENU_H
+
+#include "IMenuScreen.h"
+#include "inputState.h"
+#include "networkingManager.h"
+#include "guiManager.h"
+#include "menuType.h"
+#include <OIS/OIS.h>
+
+class StoryMenu : public IMenuScreen
+{
+    private:
+        InputState *inputState;
+        NetworkingManager *networkingMgr;
+        GuiManager *guiMgr;
+
+        CEGUI::Editbox *nickBox;
+
+        bool isVisible;
+        bool isEnd;
+        
+    public:
+        void tick();
+        bool end();
+
+        void show();
+        void hide();
+
+        bool visible();
+
+        void alphaNumKeyPressed(const OIS::KeyEvent &arg);
+
+        MenuType nextMenu();
+
+        StoryMenu(InputState *inputState, NetworkingManager *networkingMgr, GuiManager *guiMgr);
+        ~StoryMenu();
+};
+
+#endif
